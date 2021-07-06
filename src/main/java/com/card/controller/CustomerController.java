8df6c67a18044b0bad63e875c1276d9ed08d2a66
ra192 <yakovlev.roman@gmail.com>
2021-07-06 23:23:31 +0300

    add swagger

--- src/main/java/com/card/controller/CustomerController.java
+++ src/main/java/com/card/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.card.service.CustomerService;
 import com.card.service.dto.CustomerDto;
 import com.card.service.exception.CustomerException;
 import com.card.service.exception.MerchantException;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class CustomerController extends BaseController {
         this.customerService = customerService;
     }
 
+    @Operation(summary = "Register customer")
     @PostMapping("/register")
     public Customer register(@RequestBody @Valid CustomerDto customerDto) throws MerchantException, CustomerException {
         return customerService.create(customerDto);
